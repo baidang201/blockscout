4750d6d5e2c154313137f8411cf95888ddb8c408
Li Smith <jinrishouji@sina.com>
2022-05-10 12:14:18 +0800

    add highlightjs-solidity

--- apps/block_scout_web/assets/js/view_specific/address_contract/code_highlighting.js
+++ apps/block_scout_web/assets/js/view_specific/address_contract/code_highlighting.js
@@ -1,6 +1,8 @@
 import $ from 'jquery'
 import hljs from 'highlight.js'
+import hljsDefineSolidity from 'highlightjs-solidity'
 
+hljsDefineSolidity(hljs);
 // only activate highlighting on pages with this selector
 if ($('[data-activate-highlight]').length > 0) {
   hljs.initHighlightingOnLoad()
